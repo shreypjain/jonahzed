ffdef98caa62a6c7cbacb03755980d260c448647
shreypjain <jainshreyp@gmail.com>
2022-03-19 14:27:54 -0400

    small update on packages and code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import selfish from "./selfish.mp4";
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div>
+        <video
+          className="aspect-w-16 aspect-h-2"
+          src={selfish}
+          autoPlay
+          height="full"
+          loop
+          muted
+        />
+      </div>
     </div>
   );
 }
